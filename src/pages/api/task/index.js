0c636dd4c3920a7bf91dca4d22d73c85c8e4019a
rezacahyono <rezacahyono1@gmail.com>
2023-02-12 16:29:29 +0700

    finished

---
+++ src/pages/api/task/index.js
@@ -0,0 +1,33 @@
+import Task from '../../../models/Task'
+import dbConnect from '@/db/db.connect'
+
+const routes = async (req, res) => {
+  const { method } = req
+
+  await dbConnect()
+
+  if (method === 'POST') {
+    try {
+      const newTask = await new Task(req.body).save()
+      res
+        .status(201)
+        .json({ data: newTask, message: 'Task added successfully' })
+    } catch (error) {
+      res.status(500).json({ message: 'Internal Server Error' })
+      console.log(error)
+    }
+  }
+
+  if (method === 'GET') {
+    try {
+      const tasks = await Task.find()
+      res.status(200).json({ data: tasks })
+    } catch (error) {
+      res.status(500).json({ message: 'Internal Server Error' })
+      console.log(error)
+    }
+  }
+}
+
+export default routes
+
